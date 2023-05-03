3b84e175b50dda147322e93585ba879c505283d7
Harry Hands <58035341+hhands44@users.noreply.github.com>
2023-05-03 12:06:56 +0100

    first commit
    adding darwinpyspark files

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .darwinpyspark import DarwinPyspark
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="darwinpyspark",
+    version="0.0.1",
+    author="Harry Hands",
+    author_email="harry.hands@v7labs.com",
+    description="A package for interacting with the V7 platform via Pyspark",
+    url="https://github.com/v7labs/databricks/release",
+    packages=find_packages(),
+    install_requires=[
+        "requests"
+    ]
+)
